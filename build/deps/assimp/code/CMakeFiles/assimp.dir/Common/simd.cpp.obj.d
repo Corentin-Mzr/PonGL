e09cddf291b845a4513fa22c67b2c73efa2f4390
Corentin M <DeadStorm19@gmail.com>
2025-07-04 19:49:44 +0200

    Removed build and .vscode from repo

--- build/deps/assimp/code/CMakeFiles/assimp.dir/Common/simd.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-deps/assimp/code/CMakeFiles/assimp.dir/Common/simd.cpp.obj: \
- C:\Users\deads\Bureau\Jeux\github\cpp\opengl-via-submodules\deps\assimp\code\Common\simd.cpp \
- C:\Users\deads\Bureau\Jeux\github\cpp\opengl-via-submodules\deps\assimp\code\Common\simd.h \
- C:/Users/deads/Bureau/Jeux/github/cpp/opengl-via-submodules/deps/assimp/include/assimp/defs.h \
- C:/Users/deads/Bureau/Jeux/github/cpp/opengl-via-submodules/build/deps/assimp/include/assimp/config.h
--- build/deps/assimp/code/CMakeFiles/assimp.dir/__/contrib/Open3DGC/o3dgcTools.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-deps/assimp/code/CMakeFiles/assimp.dir/__/contrib/Open3DGC/o3dgcTools.cpp.obj: \
- C:\Users\deads\Bureau\Jeux\github\cpp\opengl-via-submodules\deps\assimp\contrib\Open3DGC\o3dgcTools.cpp
